98fc50b1a69dd59bbba0a12782a9b82c78d02e6d
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-05-18 19:06:18 +0800

    add some goofy files

--- main.go
+++ main.go
@@ -48,26 +48,27 @@ func (d *Disassembler) Unread(n int) (err error) {
 	return nil
 }
 
-var reg16 = map[byte]string{
-	0b000: "ax",
-	0b001: "cx",
-	0b010: "dx",
-	0b011: "bx",
-	0b100: "sp",
-	0b101: "bp",
-	0b110: "si",
-	0b111: "di",
+// rewrite using an array
+var reg16 = []string{
+	"ax",
+	"cx",
+	"dx",
+	"bx",
+	"sp",
+	"bp",
+	"si",
+	"di",
 }
 
-var reg8 = map[byte]string{
-	0b000: "al",
-	0b001: "cl",
-	0b010: "dl",
-	0b011: "bl",
-	0b100: "ah",
-	0b101: "ch",
-	0b110: "dh",
-	0b111: "bh",
+var reg8 = []string{
+	"al",
+	"cl",
+	"dl",
+	"bl",
+	"ah",
+	"ch",
+	"dh",
+	"bh",
 }
 
 func (d *Disassembler) Disassemble() (s []string, err error) {
@@ -82,8 +83,9 @@ func (d *Disassembler) Disassemble() (s []string, err error) {
 		switch {
 		case b&0b11111100 == 0b10001000: // MOV
 			{
-				d.Unread(1)
-				op, err := d.decodeMOV()
+				dFlag := b & 0b00000010 >> 1
+				wFlag := b & 0b00000001
+				op, err := d.decodeMOV(dFlag, wFlag)
 				if err != nil {
 					return s, fmt.Errorf("decode MOV: %v", err)
 				}
@@ -98,42 +100,35 @@ func (d *Disassembler) Disassemble() (s []string, err error) {
 	return s, nil
 }
 
-func (d *Disassembler) decodeMOV() (s string, err error) {
+func (d *Disassembler) decodeMOV(dFlag, wFlag byte) (s string, err error) {
 	b, err := d.Read()
 	if err != nil {
 		return "", fmt.Errorf("read byte: %v", err)
 	}
 
-	dFlag := b & 0b00000010 >> 1
-	wFlag := b & 0b00000001
-
-	b, err = d.Read()
-	if err != nil {
-		return "", fmt.Errorf("read byte: %v", err)
-	}
-
-	switch b & 0b11000000 {
-	case 0b11000000: // MOV reg, reg
+	mod := b & 0b11000000 >> 6
+	reg := b & 0b00111000 >> 3
+	rm := b & 0b00000111
+	switch mod {
+	case 0b11: // reg, reg
 		{
-			d.Unread(1)
-			return d.decodeMOVRegReg(dFlag == 1, wFlag == 1)
+			return d.decodeMOVRegReg(dFlag, wFlag, reg, rm)
+		}
+	case 0b10: // mem, reg: displacement of 16 bits
+		{
+			return d.decodeMOVMemReg(dFlag, wFlag, reg, rm)
 		}
 	}
 	return "", fmt.Errorf("invalid mov")
 }
 
-func (d *Disassembler) decodeMOVRegReg(dFlag, wFlag bool) (s string, err error) {
-	b, err := d.Read()
-	if err != nil {
-		return "", fmt.Errorf("read byte: %v", err)
-	}
-
-	reg1 := b & 0b00111000 >> 3
-	reg2 := b & 0b00000111
+func (d *Disassembler) decodeMOVRegReg(dFlag, wFlag, reg, rm byte) (s string, err error) {
+	reg1 := reg
+	reg2 := rm
 
 	var reg1str, reg2str string
 
-	if wFlag {
+	if wFlag == 1 {
 		reg1str = reg16[reg1]
 		reg2str = reg16[reg2]
 	} else {
@@ -141,11 +136,15 @@ func (d *Disassembler) decodeMOVRegReg(dFlag, wFlag bool) (s string, err error) 
 		reg2str = reg8[reg2]
 	}
 
-	if dFlag {
+	if dFlag == 1 {
 		return fmt.Sprintf("mov %s, %s", reg1str, reg2str), nil
 	} else {
 		return fmt.Sprintf("mov %s, %s", reg2str, reg1str), nil
 	}
+}
+
+func (d *Disassembler) decodeMOVMemReg(dFlag, wFlag, reg, rm byte) (s string, err error) {
+	return "", nil
 }
 
 func main() {
